afb2cd285ed18f25cc3a2e6b448b441d513221ac
jean <jeanslucasilva@gmail.comm>
2022-07-06 00:14:33 -0400

    routes adjustments

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/wddm-122-react-website" element={<Home />} />
-        <Route path="menu" element={<Menu />} />
-        <Route path="about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
 
