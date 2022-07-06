dc64b11ee710a4cd4365091fce03e466f9294376
Jean Silva <jeanslucasilva@gmail.com>
2022-07-06 09:44:51 -0300

    header links adjustments

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,13 @@ function Header() {
         <div className="h-[8vh] px-24 w-full fixed z-30 py-3 flex justify-between bg-green-900 opacity-90">
             <div className="flex-1">
                 <Link to="/">
-                    <h1 className="text-2xl font-bold text-white hover:text-yellow-200 transition-all duration-500 cursor-pointer">Coffee Cof</h1>
+                    <h1 className="text-2xl font-bold text-white hover:text-yellow-200 transition-all ease-in-out duration-500 cursor-pointer">Coffee Cof</h1>
                 </Link>
             </div>
             <div className="flex-1 flex justify-around items-center">
-                <Link to="/wddm-122-react-website" className="text-sm text-white cursor-pointer hover:text-yellow-200 transition-all duration-500"/>
-                <Link to="/wddm-122-react-website/menu" className="text-sm text-white cursor-pointer hover:text-yellow-200 transition-all duration-500"/>
-                <Link to="/wddm-122-react-website/about" className="text-sm text-white cursor-pointer hover:text-yellow-200 transition-all duration-500"/>
+                <Link to="/wddm-122-react-website" className="text-sm text-white cursor-pointer hover:text-yellow-200 transition-all ease-in-out duration-500">Home</Link>
+                <Link to="/wddm-122-react-website/menu" className="text-sm text-white cursor-pointer hover:text-yellow-200 transition-all ease-in-out duration-500">Menu</Link>
+                <Link to="/wddm-122-react-website/about" className="text-sm text-white cursor-pointer hover:text-yellow-200 transition-all ease-in-out duration-500">About</Link>
             </div>
         </div>
     )
--- src/components/menuItem.js
+++ src/components/menuItem.js
@@ -1,6 +1,6 @@
 function MenuItem(props) {
     return (
-        <div className="bg-white rounded-xl flex flex-col items-center justify-between px-4 hover:bg-gray-300 transition-all duration-500 cursor-pointer">
+        <div className="bg-white rounded-xl flex flex-col items-center justify-between px-4 hover:bg-gray-300 transition-all ease-in-out duration-500 cursor-pointer">
             <h2 className="text-2xl font-bold py-4">{props.title}</h2>
             <img src={props.img} alt="img" className="h-48 w-full object-cover" />
             <p className="py-4">{props.description}</p>
