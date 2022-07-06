b39e060e2b7a80a1d8c4a219ac22048bb1441adf
jean <jeanslucasilva@gmail.comm>
2022-07-05 23:55:11 -0400

    homepage adjustments

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,9 @@ function Header() {
     return (
         <div className="h-[8vh] px-24 w-full fixed z-30 py-3 flex justify-between bg-green-900 opacity-90">
             <div className="flex-1">
-                <h1 className="text-2xl font-bold text-white hover:text-yellow-200 transition-all duration-500 cursor-pointer">Coffee Cof</h1>
+                <a href="/">
+                    <h1 className="text-2xl font-bold text-white hover:text-yellow-200 transition-all duration-500 cursor-pointer">Coffee Cof</h1>
+                </a>
             </div>
             <div className="flex-1 flex justify-around items-center">
                 <a href="/" className="text-sm text-white cursor-pointer hover:text-yellow-200 transition-all duration-500">Home</a>
